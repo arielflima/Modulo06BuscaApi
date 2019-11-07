f5910c731cc6ecee3fe26dcb409cce6d40563a03
Ariel Lima <arielflima@gmail.com>
2019-11-07 12:37:07 -0300

    comecando api, configurando e validando

---
+++ src/pages/Main/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  ahndleAddUser = () => {
+    const { users, newUser } = this.state;
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.ahndleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
